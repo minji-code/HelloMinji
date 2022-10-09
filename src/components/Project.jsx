c1ad5e82b82a33f129184e7b4e1621b94701ebad
alswl92 <alswl92@gmail.com>
2022-10-09 11:21:00 -0400

    project added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Project.css'
 import IMG1 from '../assets/project1.png'
 import IMG2 from '../assets/project2.png'
+import IMG3 from '../assets/project3.png'
 
 
 const Project = () => {
@@ -11,6 +12,19 @@ const Project = () => {
       <h2>Projects</h2>
 
       <div className="container project_container">
+
+      <article className='project_item'>
+          <div className="project_item_wrap">
+          <div className="project_item_image">
+                <img src = {IMG3}></img>
+            </div>
+            <h4 className='project_description'>Memory Game</h4>
+            <div className='project_btn'>
+              <a href='https://github.com/alswl92/Memory_Game' className='btn' target= '_blank' rel="noopener noreferrer">Github</a>
+              <a href = 'https://alswl92.github.io/Memory_Game/' className='btn btn-primary' target= '_blank' rel="noopener noreferrer">Live Demo</a>
+            </div>
+          </div>
+          </article>
 
       <article className='project_item'>
           <div className="project_item_wrap">
@@ -34,19 +48,6 @@ const Project = () => {
             <div className='project_btn'>
               <a href='https://github.com/alswl92/MyCalculator' className='btn' target= '_blank' rel="noopener noreferrer">Github</a>
               <a href = 'https://alswl92.github.io/MyCalculator/' className='btn btn-primary' target= '_blank' rel="noopener noreferrer">Live Demo</a>
-            </div>
-          </div>
-          </article>
-
-          <article className='project_item'>
-          <div className="project_item_wrap">
-            <div className="project_item_comingsoon">
-                Something Exciting
-            </div>
-            <h4 className='project_description'>Coming Soon...</h4>
-            <div className='project_btn'>
-              <a href='https://github.com/alswl92/' className='btn' target= '_blank' rel="noopener noreferrer">Github</a>
-              <a href = 'https://github.com/alswl92/' className='btn btn-primary' target= '_blank' rel="noopener noreferrer">Live Demo</a>
             </div>
           </div>
           </article>
