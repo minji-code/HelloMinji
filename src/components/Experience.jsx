72513c178b689e6ab3d54ef544beedca832b2d2e
alswl92 <alswl92@gmail.com>
2022-10-09 21:09:53 -0400

    skills cleanup

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import cv from '../assets/Minji Kim_CV.docx'
 import{DiHtml5, DiCss3, DiJavascript, DiReact, DiGithubBadge, DiNodejs} from 'react-icons/di'
-import {FaPython, FaJava, FaFigma} from 'react-icons/fa'
-import {SiCanva, SiAdobeillustrator,} from 'react-icons/si'
+import {FaPython, FaJava} from 'react-icons/fa'
 import './Experience.css'
-import {Navigation} from 'swiper';
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
 
 const Experience = () => {
 
@@ -19,71 +14,48 @@ const Experience = () => {
       <a href ={cv} download className='btn'>Download CV</a>
       </div>
       <div className="container experience_container">
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>
-          <h3 className='l_title'>Frontend Development</h3>
-          <div className="list frontend">
-            <div className="item">
-            <DiHtml5  className='icon html'/>
-            <h4>HTML</h4>
-            </div>
-            <div className="item">
-            <DiCss3  className = 'icon css'/>
-            <h4>CSS</h4>
-            </div>
-            <div className="item">
-            <DiJavascript  className = 'icon js'/>
-            <h4>JavaScript</h4>
-            </div>
-            <div className="item">
-            <DiReact className = 'icon r'/>
-            <h4>React</h4>
-            </div>         
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <h3 className='l_title' >Backend Development</h3>
-          <div className="list backend">
-            <div className="item">
-            <FaPython className = 'icon python'/>
-            <h4>Python</h4>
-            </div>
-            <div className="item">
-            <FaJava className = 'icon java'/>
-            <h4>Java</h4>
-            </div>
-            <div className="item">
-            <DiNodejs className='icon nodejs'/>
-            <h4>Node.js</h4>
-            </div>
-            <div className="item">
-            <DiGithubBadge className='icon git'/>
-            <h4>Git</h4>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide >
-        <h3 className='l_title'>Design Tool</h3>
-          <div className="list design">
-            <div className="item">
-            <FaFigma className = 'icon figma'/>
-            <h4>Figma</h4>
-            </div>
-            <div className="item">
-            <SiCanva className = 'icon canva'/>
-            <h4>Canva</h4>
-            </div>
-            <div className="item">
-            <SiAdobeillustrator  className='icon illustrator'/>
-            <h4>Illustrator</h4>
-            </div>
-            <div className="item">
-            <div className="icon sl">sl</div>
-            <h4>Storyline360</h4>
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className="experience_content">
+        <div className="item">
+            <DiHtml5  className='icon'/>
+            <h4 className='hide'>HTML</h4>
+        </div>
+
+        <div className="item">
+            <DiCss3  className='icon'/>
+              <h4 className='hide'>CSS</h4>
+        </div>
+
+        <div className="item">
+            <DiJavascript  className='icon'/>
+              <h4 className='hide'>JavaScript</h4>
+        </div>
+
+        <div className="item">
+            <FaPython className='icon'/>
+              <h4 className='hide'>Python</h4>
+        </div>
+
+        <div className="item">
+            <FaJava className='icon'/>
+              <h4 className='hide'>Java</h4>
+        </div>
+
+        <div className="item">
+            <DiNodejs  className='icon'/>
+              <h4 className='hide'>Node.js</h4>
+        </div>
+
+        <div className="item">
+            <DiGithubBadge  className='icon'/>
+              <h4 className='hide'>Git</h4>
+        </div>
+
+        <div className="item">
+            <DiReact  className='icon'/>
+              <h4 className='hide'>React</h4>
+        </div>
+
+      </div>
       </div>
 
 
