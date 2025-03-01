7270d2a36f436efbdaf64685f4d06c04062a0446
Minji <minji.k.suh@gmail.com>
2025-03-01 17:04:27 -0500

    refactor: tailwind for home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,39 +2,38 @@ import React from 'react'
 import cv from '../assets/Minji_K_Suh_Resume.pdf'
 import ME from '../assets/profilepic.png'
 import {BsLinkedin, BsGithub} from 'react-icons/bs'
-import './Home.css'
 
 const Home: React.FC = () => {
   return ( 
-    <section className='home' id='home'>
-      <div className='page'>
-          <h3>Hello, I'm</h3>
-          <h1>Minji Kim Suh</h1>
-          <h3>Software Engineer</h3> 
-
-        <div className="container home_container">
-
-          <div className="btns">
-            <a href={cv} download className='btn'>Download Resume</a>
-            <a href="#contact" className='btn btn-primary'>Contact Me</a>
-          </div>
-
-          <div className="socials">
-            <a href="https://www.linkedin.com/in/minji-k-suh" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
-            <a href="http://github.com/minji-code" target="_blank" rel="noopener noreferrer"><BsGithub/></a>
-          </div>
-
+    <div className='page justify-center' id='home'>
+      <div className="flex flex-col items-center justify-center">
+        <div>Hello, I'm</div>
+        <h1>Minji Kim Suh</h1>
+        <h3>Software Engineer</h3> 
+      </div>
+      <div className='flex justify-center gap-4 p-8'>
+        <a href={cv} download className="btn">Download Resume</a>
+        <a href="#contact" className="btn btn-primary">Contact Me</a>
+      </div>
+      <div className='flex justify-around items-center p-8'>
+        <div className='flex flex-col text-2xl gap-4'>
+          <a href="https://www.linkedin.com/in/minji-k-suh" target="_blank" rel="noopener noreferrer">
+            <BsLinkedin />
+          </a>
+          <a href="http://github.com/minji-code" target="_blank" rel="noopener noreferrer">
+            <BsGithub />
+          </a>
+        </div>
+        <div>
           <div className="me">
             <div className="me_image">
               <img src={ME} alt="me" />
             </div>
           </div>
-
-          <h5 className='scroll_down'>Scroll Down</h5>
-          
         </div>
+        <h5 className='scroll_down'>Scroll Down</h5>
       </div>
-    </section>
+    </div>
   )
 }
 
