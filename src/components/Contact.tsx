aa7dc85f60095985e11271a822b646b44f78bbfd
Minji <minji.k.suh@gmail.com>
2025-03-01 18:13:02 -0500

    tailwind on contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, FormEvent } from 'react';
 import {AiOutlineMail, AiOutlinePhone} from 'react-icons/ai';
 import emailjs from '@emailjs/browser';
-import './Contact.css';
+// import './Contact.css';
 
 const Contact: React.FC = () => {
   const form = useRef<HTMLFormElement>(null);
@@ -24,36 +24,35 @@ const Contact: React.FC = () => {
   };
 
   return (
-    <section id='contact'>
-      <div className='page'>
+    <div className='page justify-center' id='contact'>
+      <div className='header'>
         <h3>Get In Touch</h3>
         <h2>Contact Me</h2>
+      </div>
 
-        <div className="container contact_container">
-          <div className="contact_options">
-            <article className='contact_option'>
+      <div className='flex justify-center items-center gap-24'>
+        <div className='flex flex-col gap-3'>
+          <div className='contact_option'>
               <AiOutlineMail />
               <h4>Email</h4>
               <h5>minji.k.suh@gmail.com</h5>
-              <a href="mailto:minji.k.suh@gmail.com">Send a message</a>
-            </article>
-
-            <article className='contact_option'>
+              <a className='text-[var(--color-dark)]' href="mailto:minji.k.suh@gmail.com">Send a message</a>
+          </div>
+          <div className='contact_option'>
               <AiOutlinePhone />
               <h4>Phone</h4>
               <h5>(404) 642-0400</h5>
-              <a href="tel:404-642-0400">Call Me</a>
-            </article>
+              <a className='text-[var(--color-dark)]' href="tel:404-642-0400">Call Me</a>
           </div>
-          <form ref={form} onSubmit={sendEmail}>
+        </div>
+        <form className='flex flex-col gap-4 w-[40vw]' ref={form} onSubmit={sendEmail}>
             <input type="text" name="name" placeholder="Your Full Name" required />
             <input type="email" name="email" placeholder="Your Email" required />
             <textarea name="message" rows={7} placeholder="Your Message" required/>
             <button type='submit' className='btn btn-primary'>Send Message</button>
           </form>
-        </div>
       </div>
-    </section>
+    </div>
   )
 }
 
