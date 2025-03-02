e921de49d41f477e6ead7dbf52f127e5700ddfb6
Minji <minji.k.suh@gmail.com>
2025-03-02 00:53:28 -0500

    fix: footer link hover color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ const Footer: React.FC = () => {
   return (
     <footer className='bg-[var(--color-accentB)] py-12 mt-32 w-full'>
       <div className="flex justify-center gap-8 m-8">
-        <a className='footer_socials' href="https://www.linkedin.com/in/minji-k-suh" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
-        <a className='footer_socials' href="http://github.com/minji-code" target="_blank" rel="noopener noreferrer"><BsGithub/></a>
+        <a className='footer_socials btn' href="https://www.linkedin.com/in/minji-k-suh" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
+        <a className='footer_socials btn' href="http://github.com/minji-code" target="_blank" rel="noopener noreferrer"><BsGithub/></a>
       </div>
 
       <div className='header mb-16'>
