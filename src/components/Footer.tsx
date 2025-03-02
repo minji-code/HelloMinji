0d9c27911afcaf416c6704f8a9db761c19bf7fc9
Minji <minji.k.suh@gmail.com>
2025-03-01 19:04:14 -0500

    fix: footer width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {BsLinkedin, BsGithub} from 'react-icons/bs'
 
 const Footer: React.FC = () => {
   return (
-    <footer className='bg-[var(--color-ft)] py-12 mt-32'>
+    <footer className='bg-[var(--color-ft)] py-12 mt-32 w-full'>
       <div className="flex justify-center gap-8 m-8">
         <a className='footer_socials' href="https://www.linkedin.com/in/minji-k-suh" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
         <a className='footer_socials' href="http://github.com/minji-code" target="_blank" rel="noopener noreferrer"><BsGithub/></a>
