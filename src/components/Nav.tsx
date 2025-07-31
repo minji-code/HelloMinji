afcbd19f6050395010fef53350b053e291d6c90d
Minji <minji.k.suh@gmail.com>
2025-07-30 22:37:41 -0400

    fix: nav order

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,9 +8,9 @@ const Nav: React.FC = () => {
   return (
     <nav>
       <Link to='home' spy={true} smooth={true} offset={-80} duration={500}><AiOutlineHome /></Link>
-      <Link to='project' spy={true} smooth={true} offset={-30} duration={500}><AiOutlineFundProjectionScreen /></Link>
       <Link to='experience' spy={true} smooth={true} offset={-30} duration={500}><BsFilePerson /></Link>
       <Link to='skills' spy={true} smooth={true} offset={-30} duration={500}><FaTools /></Link>
+      <Link to='project' spy={true} smooth={true} offset={-30} duration={500}><AiOutlineFundProjectionScreen /></Link>
       <Link to='contact' spy={true} smooth={true} offset={-30} duration={500}><AiOutlineMail /></Link>
     </nav>
   )
